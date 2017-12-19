2053472f4823b29bd5017e52939057e134fb3f09
Afsaneh Towhidi <afsaneh.towhidi@gmail.com>
2017-12-18 20:23:21 -0500

    After crossover - completed

--- plot.py
+++ plot.py
@@ -10,23 +10,26 @@ import csv
 
 
 def importCSV():
-	with open('output2.csv', 'rU') as infile:
+	with open('outputAfterMutation.csv', 'rU') as infile:
 		reader = csv.DictReader(infile)
 		next(reader, None)
 		data = {}
 		for row in reader:
 			for header, value in row.items():
+				if not value:
+					continue
 				try:
 					value = float(value)
 					data[header].append(value)
 				except KeyError:
 					data[header] = [value]
 
+		cr = data['cross over rate']
 		dimensions = data['dimension']
-		fitness = data['fitness value']
-		pxm = data['pxm']
-		return dimensions, fitness, pxm
-
+		mutationRate = data['mutation rate']
+		pxp = data['pxp']
+		return cr, dimensions, mutationRate, pxp
+#
 def plot(x, y , z):
 	# fig = plt.figure()
 	# ax = fig.gca(projection='3d')
@@ -38,16 +41,57 @@ def plot(x, y , z):
 
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
-	ax.plot_surface(plotx, ploty, plotz, cstride=1, rstride=1, cmap='viridis')
-	ax.set_xlabel('\n' + 'dimension', linespacing=1, labelpad=10)
-	ax.set_zlabel('\n' + 'pxm', linespacing=0.2, labelpad=10)
-	ax.set_ylabel('\n' + 'fitness value', linespacing=1, labelpad=10)
+	ax.plot_surface(plotx, ploty, plotz, cstride=1, rstride=1, linewidth=0, antialiased=False)
+	ax.set_xlabel('\n' + 'f', linespacing=1, labelpad=10)
+	ax.set_zlabel('\n' + 'p', linespacing=0.2, labelpad=10)
+	ax.set_ylabel('\n' + 'cr', linespacing=1, labelpad=10)
 	plt.show()
 
 # dimesions = data['dimension']
 # fitness = data['fitness value']
 # pxm = data['pxm']
-d, f, p = importCSV()
-plot(d, f, p)
+cr, dimensions, mutationRate, pxp = importCSV()
+plot(mutationRate, cr, pxp)
 #plot(fitness, dimesions,pxm)
 #print (fitness)
+#
+#
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+# def plot (x,y,z, d):
+# 	# # domains
+# 	# x = np.logspace(-1.,np.log10(5),50) # [0.1, 5]
+# 	# y = np.linspace(6,9,50)             # [6, 9]
+# 	# z = np.linspace(-1,1,50)            # [-1, 1]
+#
+# 	x1 = np.array(x)
+# 	y1 = np.array(y)
+# 	z1 = np.array(z)
+# 	#convert to 2d matrices
+# 	Z = np.outer(z1.T, z1)        # 50x50
+# 	X, Y = np.meshgrid(x1, y1)    # 50x50
+#
+# 	# fourth dimention - colormap
+# 	# create colormap according to x-value (can use any 50x50 array)
+# 	color_dimension = d # change to desired fourth dimension
+# 	minn, maxx = min(color_dimension), max(color_dimension)
+# 	norm = matplotlib.colors.Normalize(minn, maxx)
+# 	m = plt.cm.ScalarMappable(norm=norm, cmap='jet')
+# 	m.set_array([])
+# 	fcolors = m.to_rgba(color_dimension)
+#
+# 	# plot
+# 	fig = plt.figure()
+# 	ax = fig.gca(projection='3d')
+# 	X, Y = np.meshgrid(x, y)
+# 	ax.plot_surface(X,Y,Z, rstride=1, cstride=1, facecolors=fcolors, vmin=minn, vmax=maxx, shade=False)
+# 	ax.set_xlabel('x')
+# 	ax.set_ylabel('y')
+# 	ax.set_zlabel('z')
+# 	fig.canvas.show()
+#
+# cr, dimensions, mutationRate, pxp = importCSV()
+# plot(cr, pxp,mutationRate, dimensions)
--- point.py
+++ point.py
@@ -3,7 +3,6 @@ import scipy as sc
 from scipy import spatial
 import time
 import csv
-from matplotlib import pyplot
 
 
 class Point:
@@ -12,10 +11,18 @@ class Point:
 		self.value = 0
 		self.name = name
 		self.distance = 0
-		self.counter = 0
+		self.afterMutationCounter = 0
+		self.afterCrossoverCounter = 0
+		self.initialCounter = 0
 
-	def addCounter(self):
-		self.counter += 1
+	def addinitialCounter(self):
+		self.initialCounter += 1
+
+	def addAfterMutationCounter(self):
+		self.afterMutationCounter += 1
+
+	def addAfterCrossoverCounter(self):
+		self.afterCrossoverCounter += 1
 
 	def setDistance(self, dist):
 		self.distance = dist
@@ -27,8 +34,17 @@ class Point:
 		"""assign a new value to a point in d dimension"""
 		self.value = np.random.rand(1, self.dimension)[0][0]
 
+	def copyObject(self, y):
+		self.dimension = y.dimension
+		self.value = y.value
+		self.name = y.name
+		self.distance = y.distance
+		self.afterMutationCounter = y.afterMutationCounter
+		self.afterCrossoverCounter = y.afterCrossoverCounter
+		self.initialCounter = y.initialCounter
+
 	def __repr__(self):
-		return '%s --> distance: %s \n\t   counter: %s' % (self.name, self.distance, self.counter)
+		return '%s --> distance: %s \n\t   counter: %s %s' % (self.name, self.distance, self.afterMutationCounter,self.afterCrossoverCounter)
 
 	def __lt__(self, other):
 		return self.distance < other.distance
@@ -42,10 +58,12 @@ def findDistance(x, y):
 	return sc.spatial.distance.euclidean(x.value, y.value)
 
 
-def findMinimumDistancePoint(s, xa, xb, xc, xm):
-	'''find the point with the minimum distance'''
-
-	dictionary = {xa: xa.value, xb: xb.value, xc: xc.value, xm: xm.value}
+def findMinimumDistancePoint(s, pointLst):
+	"""find the point with the minimum distance"""
+	dictionary = {}
+	for x in pointLst:
+		dictionary[x] = x.value
+	#dictionary = {xa: xa.value, xb: xb.value, xc: xc.value, xm: xm.value, xp: xp.value}
 	minimumValue = min(dictionary, key=dictionary.get)
 	return minimumValue
 
@@ -58,37 +76,51 @@ def buildPoints(d):
 	xb = Point('xb',d)
 	xc = Point('xc',d)
 	xm = Point('xm', d)
-	return s, xa, xb, xc, xm
+	xp = Point('xp', d)
+	return s, xa, xb, xc, xm, xp
 
 
-def buildTheCounters(d, f):
-	'''count the number of times each point has a minimum dist to the solution'''
-	s, xa, xb, xc, xm = buildPoints(d)
-	for i in range(1, 100001):
+def buildTheCounters(d, f, cr):
+	"""count the number of times each point has a minimum dist to the solution"""
+
+	s, xa, xb, xc, xm, xp = buildPoints(d)
+
+	for i in range(100000):
 		xa.setRandomValueToPoint()
 		xb.setRandomValueToPoint()
 		xc.setRandomValueToPoint()
 		xm.setValue(xa.value + f * (xb.value - xc.value))
+		xp.setRandomValueToPoint()
 
 		xa.setDistance(findDistance(s, xa))
 		xb.setDistance(findDistance(s, xb))
 		xc.setDistance(findDistance(s, xc))
 		xm.setDistance(findDistance(s, xm))
+		xp.setDistance(findDistance(s, xp))
 
 
-		# print(findMinimumDistancePoint(s, xa, xb, xc, xm))
-		findMinimumDistancePoint(s, xa, xb, xc, xm).addCounter()
-		# print(xa.counter, xb.counter, xc.counter)
-		# print(xa.value, xb.value, xc.value)
-	return s, xa, xb,xc, xm
+		#------------------------------------------------------
+		#initial points
+		findMinimumDistancePoint(s, [xa, xb, xc, xp]).addinitialCounter()
+
+		#------------------------------------------------------
+		#mutation
+		findMinimumDistancePoint(s, [xa, xb, xc, xm, xp]).addAfterMutationCounter()
+
+		# ------------------------------------------------------
+		#crossover
+		if isCrossOver(cr):
+			if xm.distance < xp.distance:
+				xp.copyObject(xm)
+				findMinimumDistancePoint(s, [xa, xb, xc, xp]).addAfterCrossoverCounter()
+			else:
+				findMinimumDistancePoint(s, [xa, xb, xc, xp]).addAfterCrossoverCounter()
+
+	return s, xa, xb, xc, xm, xp
 
 
 def probability(counter, total):
 	return counter/total
-
-
-def pointProbability(x, total):
-	return probability(x.counter, total)
 
 
 def setXmValue(f, xa, xb, xc, xm):
@@ -102,27 +134,99 @@ def isCrossOver(cr):
 	else:
 		return False
 
+# def main():
+#
+# 	# run for 100'000 times
+#
+# 	# build solution and points
+#
+# 	# ----------------------------------------------
+# 	# Calculate the probability before crossover
+#
+#
+# 	#----------------------------------------------
+# 	# Do the crossover
+#
+# 	#----------------------------------------------
+# 	# Calculate the probability after crossover
+#
+#
+# 	return
 
-def test():
+
+def main():
 	start_time = time.time()
-	fitness = [0.4, 0.8, 1]
-	csvFile = open('output.csv', 'a')
-	rowWriter = csv.writer(csvFile, delimiter=',')
-	rowWriter.writerow(['dimension', 'fitness value', 'pxa', 'pxb', 'pxc', 'pxm'])
+	#fitness = [0.4, 0.8, 1]
 
-	for f in fitness:  # to check different fitness values
-		for d in range(1, 100):  # dimension interval
-			s, xa, xb, xc, xm = buildTheCounters(d, f)
+	#file 1
+	csvFile1 = open('outputinitialPoints.csv', 'w')
+	writer1 = csv.writer(csvFile1, delimiter=',')
+	writer1.writerow(['dimension', 'mutation rate', 'cross over rate', 'pxa', 'pxb', 'pxc', 'pxp'])
 
-			pxa = pointProbability(xa, 100000)
-			pxb = pointProbability(xb, 100000)
-			pxc = pointProbability(xc, 100000)
-			pxm = pointProbability(xm, 100000)
+	#file 2
+	csvFile2 = open('outputAfterMutation.csv', 'w')
+	writer2 = csv.writer(csvFile2, delimiter=',')
+	writer2.writerow(['dimension', 'cross over rate', 'mutation rate', 'pxa', 'pxb', 'pxc', 'pxp', 'pxm'])
 
-			sentence = [d, f, pxa, pxb, pxc, pxm]
-			rowWriter.writerow(sentence)
-	csvFile.close()
+	#file 3
+	csvFile3 = open('outputAfterCrossover.csv', 'w')
+	writer3 = csv.writer(csvFile3, delimiter=',')
+	writer3.writerow(['dimension', 'cross over rate', 'mutation rate', 'pxa', 'pxb', 'pxc', 'pxp'])
+
+
+	crossOver = [float(("{0:.1f}".format(i))) for i in np.arange(0.1, 1, 0.1)]
+	fitness = [float(("{0:.1f}".format(i))) for i in np.arange(0.2, 1.1, 0.1)]
+	# crossOver = [float(("{0:.1f}".format(i))) for i in np.arange(0.1, 1, 0.3)]
+	# fitness = [float(("{0:.1f}".format(i))) for i in np.arange(0.2, 1.1, 0.4)]
+	dimension = [i for i in range(1, 101)]
+
+	for d in dimension:  # dimension interval
+		print ("dimension", d)
+		for cr in crossOver:
+			print ("cr", cr)
+			for f in fitness:  # to check different fitness values
+				s, xa, xb, xc, xm, xp = buildTheCounters(d, f, cr)
+
+				#probabilities of initial points
+				pxa3 = probability(xa.initialCounter, 100000)
+				pxb3 = probability(xb.initialCounter, 100000)
+				pxc3 = probability(xc.initialCounter, 100000)
+				pxp3 = probability(xp.initialCounter, 100000)
+
+				#probabilities after mutation
+				pxa1 = probability(xa.afterMutationCounter, 100000)
+				pxb1 = probability(xb.afterMutationCounter, 100000)
+				pxc1 = probability(xc.afterMutationCounter, 100000)
+				pxp1 = probability(xp.afterMutationCounter, 100000)
+				pxm1 = probability(xm.afterMutationCounter, 100000)
+
+				#probabilities after crossover
+				pxa2 = probability(xa.afterCrossoverCounter, 100000)
+				pxb2 = probability(xb.afterCrossoverCounter, 100000)
+				pxc2 = probability(xc.afterCrossoverCounter, 100000)
+				pxp2 = probability(xp.afterCrossoverCounter, 100000)
+				#pxm2 = probability(xp.afterCrossoverCounter, 100000)
+
+
+				#initial point file writing
+				sentenceInitialPoints = [d, cr, f, pxa3, pxb3, pxc3, pxp3]
+				writer1.writerow(sentenceInitialPoints)
+
+				#after mutation file writing
+				sentenceAfterMutation = [d, cr, f, pxa1, pxb1, pxc1, pxp1, pxm1]
+				writer2.writerow(sentenceAfterMutation)
+				#print("--- %s seconds for the first file---" % (time.time() - start_time))
+
+
+				#after crossover file writing
+				sentenceAfterCrossover = [d, cr, f, pxa2, pxb2, pxc2, pxp2]
+				writer3.writerow(sentenceAfterCrossover)
+
+	csvFile1.close()
+	csvFile2.close()
+	csvFile3.close()
 
 	print("--- %s seconds ---" % (time.time() - start_time))
 
-test()
+main()
+
